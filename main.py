8f1b06eab4937c612489894ebe06b1acf1df36d7
bengrunfeld <benjamin.grunfeld@gmail.com>
2014-10-22 18:20:47 -0700

    Nightly commit. Bug exists in routes

--- main.py
+++ main.py
@@ -1,19 +1,37 @@
-import webapp2
 import json
+import webapp2
+
+from webapp2_extras import routes
 
 
 class GetAllTodos(webapp2.RequestHandler):
     def get(self):
         """Returns a JSON formatted greeting"""
+
+        # TODO: Should retrieve data from a datastore
         greeting = {'greeting': 'Hello there'}
         json.dumps(greeting, sort_keys=True, indent=4)
 
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.write(json.dumps(greeting))
 
+class GetOneTodo(webapp2.RequestHandler):
+    def get(self):
+        """Returns a JSON formatted greeting"""
+
+        # TODO: Should retrieve data from a datastore
+        greeting = {'greeting': 'Hello there'}
+        json.dumps(greeting, sort_keys=True, indent=4)
+
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write(json.dumps(greeting))
+
+
 app = webapp2.WSGIApplication([
-    webapp2.Route(r'/todos/api/v0.1.0', 
-                  handler=GetAllTodos, 
-                  name='get-all-todos',
-                  methods=['GET']),
+    routes.PathPrefixRoute('/todos/api/v0.1.0', [
+        webapp2.Route('<tod_id>',
+                      handler=GetAllTodos,
+                      name='get-all-todos',
+                      methods=['GET']),
+    ]),
 ])
